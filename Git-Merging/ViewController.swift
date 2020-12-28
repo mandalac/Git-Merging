a9d5eb779d4013b13bc5c13047343b24b66fe752
Vasili Mandalac <mandalacvasili1991@gmail.com>
2020-12-28 15:53:03 -0600

    Initial Commit

---
+++ Git-Merging/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Git-Merging
+//
+//  Created by Vasili Mandalac on 12/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
